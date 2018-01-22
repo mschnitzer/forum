11070ff93439dfb83b61908a7c0e23abaa6a8d6a
Manuel Schnitzer <webmaster@mschnitzer.de>
2018-01-22 21:45:22 +0100

    Add version number

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Forum
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Application Version
+    VERSION = "1.0.0-Alpha1"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
