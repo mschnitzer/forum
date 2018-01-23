8b371eb831d8fed34793e3adb0aa1a217ff010e3
Manuel Schnitzer <webmaster@mschnitzer.de>
2018-01-23 16:40:03 +0100

    Add seeds for user groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Group.create!([
+  { name: 'Administrators' },
+  { name: 'Moderators' },
+  { name: 'Registered Users', default_group: true },
+  { name: 'Guests', guest_group: true }
+])
