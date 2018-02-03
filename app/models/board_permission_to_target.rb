c0f1ba1408e8c991f7d1c4cf254f92df4a4a562b
Manuel Schnitzer <webmaster@mschnitzer.de>
2018-02-04 00:10:19 +0100

    Require either user or group to be set for board permissions

--- app/models/board_permission_to_target.rb
+++ app/models/board_permission_to_target.rb
@@ -1,6 +1,15 @@
 class BoardPermissionToTarget < ApplicationRecord
   belongs_to :permission, class_name: 'BoardPermission'
   belongs_to :board
-  belongs_to :user
-  belongs_to :group
+  belongs_to :user, optional: true
+  belongs_to :group, optional: true
+
+  validate :user_or_group_validation
+
+  private
+
+  def user_or_group_validation
+    errors.add(:base, 'User or Group must be set') unless user || group
+    errors.add(:base, 'either User or Group can be set') if user && group
+  end
 end
