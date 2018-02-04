09b3ea5f94857ad953cfba071eb71db52f44f264
Manuel Schnitzer <webmaster@mschnitzer.de>
2018-02-04 01:49:12 +0100

    Add specs for groups in board_policy_spec.rb

---
+++ spec/factories/group.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :group do
+    sequence :name { |n| "group_#{n}" }
+  end
+end
--- spec/policies/board_policy_spec.rb
+++ spec/policies/board_policy_spec.rb
@@ -1,6 +1,7 @@
 describe BoardPolicy do
   subject { described_class }
   let(:user) { create(:user) }
+  let(:group) { create(:group) }
   let(:board) { create(:board) }
 
   permissions :show? do
@@ -14,12 +15,34 @@ describe BoardPolicy do
           expect(subject).not_to permit(user, board)
         end
       end
+
+      context 'for groups' do
+        before do
+          group.board_permission_to_targets.create!(board: board, permission: BoardPermission.find_by!(name: 'can_access_board'), value: false)
+          user.user_to_groups.create!(group: group)
+        end
+
+        it 'board permission :can_access_board is denied' do
+          expect(subject).not_to permit(user, board)
+        end
+      end
     end
 
     context 'access granted' do
       context 'for users' do
         before do
           user.board_permission_to_targets.create!(board: board, permission: BoardPermission.find_by!(name: 'can_access_board'), value: true)
+        end
+
+        it 'board permission :can_access_board is allowed' do
+          expect(subject).to permit(user, board)
+        end
+      end
+
+      context 'for groups' do
+        before do
+          group.board_permission_to_targets.create!(board: board, permission: BoardPermission.find_by!(name: 'can_access_board'), value: true)
+          user.user_to_groups.create!(group: group)
         end
 
         it 'board permission :can_access_board is allowed' do
